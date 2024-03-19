5fd1a4860b83c671f18e61bbf29264f6ec7d25cb
Lumajo22 <163960099+Lumajo22@users.noreply.github.com>
2024-03-19 16:36:32 -0500

    Add files via upload

---
+++ Parcial/scriptabaout.js
@@ -0,0 +1,4 @@
+document.getElementById('togglePages').addEventListener('click', function() {
+    var additionalPages = document.getElementById('additionalPages');
+    additionalPages.classList.toggle('active'); // Activa o desactiva la clase 'active' en las páginas adicionales
+});
